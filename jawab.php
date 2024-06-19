e3e18e128b31b8643303122502e99201ae61575f
zeptink <72772064+zeptink@users.noreply.github.com>
2024-06-19 12:55:13 +0700

    Add files via upload

---
+++ jawab.php
@@ -0,0 +1,6 @@
+<?php
+echo "<center>Jenis Kelamin: ".$_POST['JenisKelamin']."</center><br>";
+echo "<center>Usia: ".$_POST['usia']."</center><br>";
+echo "<center>Jurusan yang dipilih: ".$_POST['jurusan']."</center><br>";
+echo "<center>Alasan: ".$_POST['alasan']."</center><br>";
+?>
